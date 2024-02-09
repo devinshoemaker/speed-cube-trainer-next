bc9f5d8fc9393208904d61c8845548e9eb991b7b
Devin Shoemaker <devinshoe@gmail.com>
2024-02-09 12:11:15 -0600

    chore: enforce auth guard in app layout (#1)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,19 @@
+import { redirect } from 'next/navigation';
 import React from 'react';
 
+import { auth } from '@/auth';
 import SideMenu from '@/components/side-menu';
 
-export default function SideMenuLayout({
+export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await auth();
+  if (!user) {
+    redirect('/login');
+  }
+
   return (
     <div className="relative min-h-screen md:flex w-full">
       <SideMenu />
