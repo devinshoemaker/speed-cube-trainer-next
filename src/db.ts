5031f2a6c811c20060a31e37fc3f9fcbd1229721
Devin Shoemaker <devinshoe@gmail.com>
2024-04-23 15:41:40 -0500

    [WIP] chore: migrate to SQLite

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,19 @@
+import { createClient } from '@libsql/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
+  if (process.env.TURSO_DATABASE_URL) {
+    const libsql = createClient({
+      url: process.env.TURSO_DATABASE_URL!,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    });
+
+    const adapter = new PrismaLibSQL(libsql);
+    const prisma = new PrismaClient({ adapter });
+    return prisma;
+  }
+
   return new PrismaClient();
 };
 
